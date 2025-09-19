5d289eb13b4a6f1cdbc431852f3a472e9953424c
Flex-Lucky <flex.lucky555@gmail.com>
2025-09-19 18:03:10 +0000

    chor: .

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 import { ChakraProvider } from "@chakra-ui/react";
-import QueryProvider from 'src/contexts/QueryProvider'
+import QueryProvider from "src/contexts/QueryProvider";
 // import ThemeProvider from "src/contexts/ThemeProvider";
 import Loading from "src/components/Loading";
 import App from "src/AppWrapper";
 import "src/index.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
-    <QueryProvider>
-      {/* <ThemeProvider> */}
-      <Suspense fallback={<Loading />}>
-        <ToastContainer />
-        <App />
-      </Suspense>
-      {/* </ThemeProvider> */}
-    </QueryProvider>
-  </ChakraProvider>
-) 
+    <ChakraProvider>
+        <QueryProvider>
+            <Suspense fallback={<Loading />}>
+                <ToastContainer />
+                <App />
+            </Suspense>
+        </QueryProvider>
+    </ChakraProvider>
+);
